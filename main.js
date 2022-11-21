957b607dc7c30134e5c110b4d628a36e937745cb
Rodrigo <rodrigo1cejas@gmail.com>
2022-11-21 20:17:48 -0300

    actualizacion

---
+++ main.js
@@ -0,0 +1,14 @@
+const pizzas = [
+    {id: 1, name: "Mozzarella", ingredientes: ["queso muzzarela", "salsa de tomate", "aceitunas"], precio: 500},
+    {id: 2, name: "Especial", ingredientes: ["queso muzzarela", "salsa de tomate", "jamón cocido", "morrón"], precio: 530},
+    {id: 3, name: "Fugazzetta", ingredientes: ["queso muzzarela", "queso cuartirolo", "cebolla"], precio: 640},
+    {id: 4, name: "Napolitana", ingredientes: ["queso muzzarela", "salsa de tomate", "rodajas de tomate", "albahaca"], precio: 580},
+    {id: 5, name: "Calabresa", ingredientes: ["queso muzzarella", "salsa de tomate", "longaniza calabresa"], precio: 620},
+    {id: 6, name: "de Rúcula", ingredientes: ["queso muzzarella", "queso parmesano", "rúcula", "jamón crudo"], precio: 650},
+    {id: 7, name: "Marinara", ingredientes: ["salsa de tomate", "orégano", "ajo", "albahaca"], precio: 550}
+]
+
+
+let numberInput = document.getElementById('pizzaId__input')
+
+console.log(numberInput)
